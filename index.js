b70de8d543aa0e00befd229968ec002903e07bd3
Joanna Lin <lin.joanna.y@gmail.com>
2020-05-18 20:48:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, call) {
+  let finalArray = []
+  for (let i = 0; i < array.length; i++) {
+    finalArray.push(call(array[i]))
+  }
+  return finalArray
+}
+
+function reduce(array, call, start){
+  let r = (!!start) ? start : array[0]
+  let i = (!!start) ? 0 : 1
+
+  for (; i < array.length; i++) {
+    r = call(array[i], r)
+  }
+
+  return r;
+}
